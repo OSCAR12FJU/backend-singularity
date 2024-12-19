ee00bb907572ef099f4a8c5e4a7a85c47aa10e32
Oscar Flores <oscar.blv.cabj@hotmail.com>
2024-12-19 15:59:35 -0300

    feat(config): add new configurations for database connection

--- eslint.config.js
+++ eslint.config.js
@@ -5,8 +5,14 @@ import pluginReact from "eslint-plugin-react";
 
 /** @type {import('eslint').Linter.Config[]} */
 export default [
-  {files: ["**/*.{js,mjs,cjs,jsx}"]},
-  {languageOptions: { globals: globals.browser }},
+  {
+    files: ["**/*.{js,mjs,cjs,jsx}"]
+  },
+  {
+    languageOptions: { 
+      globals: globals.browser
+     }
+  },
   pluginJs.configs.recommended,
   pluginReact.configs.flat.recommended,
 ];
--- index.js
+++ index.js
@@ -2,10 +2,11 @@ import express from 'express';
 import authRoutes from './src/app/routes/authRoutes.js';
 import userRoutes from './src/app/routes/userRoutes.js'
 import cors from 'cors'
-
+import dotenv from 'dotenv'
 
 const app = express();
 
+dotenv.config();
 app.use(express.json());
 app.use(cors());
 
@@ -13,7 +14,8 @@ app.use(cors());
 app.use('/api/auth', authRoutes);
 app.use('/api/users', userRoutes);
 
-const PORT = 3000;
-app.listen(PORT, ()=>{
+
+const PORT = process.env.PORT
+app.listen(PORT, () => {
     console.log(`Servidor corriendo en http://localhost:${PORT}`);
-})
+});
